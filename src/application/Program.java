d4616813eb26e935b2be537be003bb07598834e0
Nicole Bauchspiess <nicoleb3007@gmail.com>
2023-05-01 16:11:29 -0300

    Criado classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 
 		Department d = new Department(1,"book");
 		System.out.println(d);
+		
+		Seller s= new Seller(21, "Bob", "bob@gmail.com", new Date(), 200.0, d);
+		System.out.println(s);
 		
 	}
 
